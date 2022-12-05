5c1780c3872d0edc4edcc07975c5dba837d2c436
David Fdez <dav.fdez.lopez@gmail.com>
2022-12-05 17:24:33 +0100

    services

---
+++ src/services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+class AuthService {
+
+    constructor() {
+
+        this.api = axios.create({
+            baseURL: `${process.env.REACT_APP_API_URL}/auth`
+        })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    signup(userData) {
+        return this.api.post('/signup', userData)
+    }
+
+    login(userData) {
+        return this.api.post('/login', userData)
+    }
+
+    verify = token => {
+        return this.api.get('/verify', { headers: { Authorization: `Bearer ${token}` } })
+    }
+}
+
+const authService = new AuthService()
+
+export default authService
