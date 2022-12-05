46cf23303736d468b1e172c0c548eb815ee63f5a
Marta Salvador <martasbsv@gmail.com>
2022-12-05 17:26:32 +0100

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppRoutes from './routes/AppRoutes'
+
 
 function App() {
   return (
     <div className="App">
       <h1>SUPER WEB DE RECETAS</h1>
+      <AppRoutes />
     </div>
   );
 }
