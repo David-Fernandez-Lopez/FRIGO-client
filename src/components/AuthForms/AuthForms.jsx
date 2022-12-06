491822b1353f1a89b7e46b311ad6e909a216fa51
Marta Salvador <martasbsv@gmail.com>
2022-12-06 13:09:03 +0100

    css forms

--- src/components/AuthForms/AuthForms.jsx
+++ src/components/AuthForms/AuthForms.jsx
@@ -3,15 +3,17 @@ import { Tab, Tabs } from "react-bootstrap"
 import LogInForm from "../LogInForm/LogInForm"
 import SignUpForm from "../SignUpForm/SignUpForm"
 
+import './AuthForms.css'
+
 
 const AuthForms = () => {
 
     return (
-            <Tabs defaultActiveKey="Forms" id="fill" className="mb-3" justify>
-                <Tab eventKey="login" title="Log In">
+            <Tabs defaultActiveKey="Forms" id="fill" className="forms mb-3" justify>
+                <Tab className="login" eventKey="login" title="Log In">
                     <LogInForm />
                 </Tab>
-                <Tab eventKey="signup" title="Sign Up">
+                <Tab className="signup" eventKey="signup" title="Sign Up">
                     <SignUpForm />
                 </Tab>
             </Tabs>
