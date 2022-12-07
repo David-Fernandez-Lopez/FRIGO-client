1a64c53cf63b3519b0d4d86921bf651dac9e4bf3
Marta Salvador <martasbsv@gmail.com>
2022-12-07 17:44:58 +0100

    cards homepage in process

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,5 +1,6 @@
 import './CategoryCard.css'
 import { Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 
 function CategoryCard({ title, image, id }) {
@@ -7,16 +8,17 @@ function CategoryCard({ title, image, id }) {
     return (
 
         <div className="CategoryCard me-2">
+            <Link>
             <Card className="bg-ligth text-black">
                 <Card.Img src={image} alt="Card image" />
                 <Card.ImgOverlay>
                     <Card.Title>Icons</Card.Title>
+                    <p>{title}</p>
                 </Card.ImgOverlay>
                 <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
+            </Link>
         </div>
     )
 
