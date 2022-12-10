edbc153352401bf1df088953d30121b89b4d973e
Marta Salvador <martasbsv@gmail.com>
2022-12-10 14:29:02 +0100

    about us page

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -5,8 +5,11 @@ import '../About/AboutPage.css'
 const AboutPage = () => {
 
     return (
-    <Container>
-            <h1 className='aboutPageTitle'>About Page</h1>
+        <Container className="aboutPage">
+            <div>
+                <img src='https://images.unsplash.com/photo-1652358357453-25ae54e2e98a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' alt='team image' />
+                <h1 className='aboutPageTitle'>About Us</h1>
+            </div>
             <hr />
             <h3 className='aboutTitles'>Who are we & What is FRIGO?</h3>
     </Container>
