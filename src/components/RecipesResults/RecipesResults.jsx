b4d6decc372feea30ece145f13b0be4c73555208
Marta Salvador <martasbsv@gmail.com>
2022-12-13 13:58:20 +0100

    styled card of homepage

--- src/components/RecipesResults/RecipesResults.jsx
+++ src/components/RecipesResults/RecipesResults.jsx
@@ -1,22 +1,20 @@
 import './RecipesResults.css'
-import { Card} from 'react-bootstrap'
+import { Card, Container, Row} from 'react-bootstrap'
 import { elementAcceptingRef } from '@mui/utils'
 import { Link } from 'react-router-dom'
+import { ConfirmationNumber } from '@mui/icons-material'
 
 const RecipesResults = ({ recipesToSearch }) => {
 
     return (
         <>
-            {recipesToSearch && <section className="CountriesList" style={{ "maxHeight": "40vh"}}>
+            {recipesToSearch && <section className="recipeResults">
                 {recipesToSearch.map((elm, idx) => {
                     return (
-                     
                         <Link  key={idx} to={`/recipes/${elm.id}/information`}>
-                            <Card className="bg-ligth text-black">
-                                    <h5>{elm.title}</h5>
+                            <Card className="recipeResultsCard">
                                 <Card.Img className='imgResult' src={elm.image} alt="Card image" />
-                                <Card.ImgOverlay>
-                                </Card.ImgOverlay>
+                                <p className='recipeRsultsT'>{elm.title}</p>
                             </Card>
                         </Link>
                     )
