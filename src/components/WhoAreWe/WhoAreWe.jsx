19466ac8389e1127d78bde014da04249faf37d0d
David Fdez <dav.fdez.lopez@gmail.com>
2022-12-15 16:01:41 +0100

    recipe steps,navitation, aboutUs

--- src/components/WhoAreWe/WhoAreWe.jsx
+++ src/components/WhoAreWe/WhoAreWe.jsx
@@ -13,11 +13,19 @@ const WhoAreWe = () => {
             <h3 className='WhoAreWeT'>Who are we?</h3>
             <Row className="teamRow mb-4">
                 <Col xs={5}>
-                    <img className="personalImgJ" src="https://res.cloudinary.com/dp0abawuh/image/upload/v1670944890/jadde_mteitb.jpg" alt="dav" />
+                    <img className="personalImgJ"
+                        src="https://res.cloudinary.com/dp0abawuh/image/upload/v1670944890/jadde_mteitb.jpg"
+                        alt="dav" />
                 </Col>
                 <Col xs={5}>
                     <h4 className="teamName">Jadde Suárez</h4>
-                    <p style={{ textAlign: "justify" }}>lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.</p>
+                    <p style={{ textAlign: "justify" }}>lorem Ipsum es simplemente el texto de relleno
+                        de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno
+                        estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
+                        que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de
+                        tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años,
+                        sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando
+                        esencialmente igual al original.</p>
                      <a href="https://www.linkedin.com/in/jaddesuarez/" target="_blank">
                         <LinkedInIcon /> <span><LocalPhoneIcon/>+67890</span>
                     </a>
@@ -26,22 +34,35 @@ const WhoAreWe = () => {
             <Row className="teamRow mb-4">
                 <Col xs={5}>
                     <h4 className="teamName">David Fernández</h4>
-                    <p style={{ textAlign: "justify" }}>lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.</p>
+                    <p style={{ textAlign: "justify" }}>My journey through life has gone through many stages.
+                        Always studying and working at the same time, I have knowledge in Forest Management
+                        and Environmental Health, which allowed me to work for several years as an Environmental
+                        Quality Auditor. Now, determined to give my life a 180 degree turn, I have added Web Development
+                        to my skills. I hope it's for the best.</p>
                      <a href="https://www.linkedin.com/in/davidfernandezlopez/" target="_blank">
-                        <LinkedInIcon /> <span><LocalPhoneIcon/>+67890</span>
+                        <LinkedInIcon /> <span><LocalPhoneIcon/>+34 670629076</span>
                     </a>
                 </Col>
                 <Col xs={5}>
-                    <img className="personalImgD" src="https://res.cloudinary.com/dp0abawuh/image/upload/v1671093853/SquareQuick_20221213163554460-01_nihktc.jpg" alt="dav" />
+                    <img className="personalImgD"
+                        src="https://res.cloudinary.com/dp0abawuh/image/upload/v1671093853/SquareQuick_20221213163554460-01_nihktc.jpg"
+                        alt="dav" />
                 </Col>
             </Row>
             <Row className="teamRow mb-4">
                 <Col xs={5}>
-                    <img className="personalImgM" src="https://res.cloudinary.com/dp0abawuh/image/upload/v1670944892/marta_aow4i3.jpg" alt="dav" />
+                    <img className="personalImgM"
+                        src="https://res.cloudinary.com/dp0abawuh/image/upload/v1670944892/marta_aow4i3.jpg"
+                        alt="dav" />
                 </Col>
                 <Col xs={5}>
                     <h4 className="teamName">Marta Salvador</h4>
-                    <p style={{ textAlign: "justify" }}>Apasionada del cine, decidí estudiar Comunicación Audiovisual en Madrid. Tras unos años colaborando en proyectos cinematográficos y publicitarios, decido buscar nuevas opciones que me motiven y me topo con el Diseño y Experiencia de Usuario. Ahora, para ampliar mis conocimientos, he decidido embarcarme, sin conocimientos previos en código, en la experiencia de un Bootcamp de Full Stack del que salgo emocionada por las posibilidades de la programación y con ganas de seguir mejorando.</p>
+                    <p style={{ textAlign: "justify" }}>Apasionada del cine, decidí estudiar Comunicación
+                        Audiovisual en Madrid. Tras unos años colaborando en proyectos cinematográficos y
+                        publicitarios, decido buscar nuevas opciones que me motiven y me topo con el Diseño
+                        y Experiencia de Usuario. Ahora, para ampliar mis conocimientos, he decidido embarcarme,
+                        sin conocimientos previos en código, en la experiencia de un Bootcamp de Full Stack del
+                        que salgo emocionada por las posibilidades de la programación y con ganas de seguir mejorando.</p>
                     <a href="https://www.linkedin.com/in/martasalvadorberenguer/" target="_blank">
                         <LinkedInIcon /> 
                     </a>
